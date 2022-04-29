68a9761905e03499aba7d53bc53b79811defd84f
Victor Emanuel <victor_emanuell@yahoo.com.br>
2022-04-28 22:08:19 -0300

    arquivos copiados

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -7,4 +7,6 @@ wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1ae
 apt-get install zip
 apt-get install unzip
 unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
-
+rm /var/www/html/index.html
+cp /root/p1sistema/873219-690e1f604dc8ead2583589e1aee6f8a42040a035/cv-resumido.html /var/www/html/
+cp /root/p1sistema/873219-690e1f604dc8ead2583589e1aee6f8a42040a035/style.css /var/www/html/
