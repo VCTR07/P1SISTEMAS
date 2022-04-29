d9c6020b4d2530742e20d96263aa0e105f2134e9
Victor Emanuel <victor_emanuell@yahoo.com.br>
2022-04-28 21:25:55 -0300

    descompatando arquivo

---
+++ configura_servidorWeb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade
+apt-get install apache2
+apt-get install wget
+wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+apt-get install zip
+apt-get install unzip
+unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
